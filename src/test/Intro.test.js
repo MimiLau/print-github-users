39591dd055bdde6da3c1fb4fe64a7a94e5b0bed8
Mimi Lau <mimi@aftership.net>
2016-12-01 11:51:30 +0800

    bug fixes

--- src/test/Intro.test.js
+++ src/test/Intro.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Intro from './Intro';
+import Intro from '../components/Intro';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
